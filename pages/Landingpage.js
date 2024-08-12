24a4637bea185a53d68bcc2b773449257c18d19f
harshblip <harsh.jipkate2020@vitbhopal.ac.in>
2024-08-12 22:14:56 +0530

    homepage made me

--- pages/Landingpage.js
+++ pages/Landingpage.js
@@ -5,9 +5,9 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import DefaultLayout from '../layout/default'
 import Image from 'next/image'
-import { Raleway } from 'next/font/google';
+import { Raleway, Kanit, Comfortaa } from 'next/font/google';
 import styles from '../src/app/Landingpage.module.css'
-import { Map, Keyboard } from 'lucide-react';
+import { Map, Keyboard, Instagram, Twitter, Facebook, Mail } from 'lucide-react';
 import text from '../dumbdata.json'
 import { storeCity, storeCoord } from '../slices/locationSlice'
 import { useDispatch } from 'react-redux'
@@ -18,15 +18,30 @@ const railway = Raleway({
     subsets: ['latin']
 })
 
+const kanit = Kanit({
+    weight: ['200', '300'],
+    subsets: ['latin']
+})
+
+const comfortaa = Comfortaa({
+    subsets: ['latin'],
+    display: 'swap',
+    weight: ['300', '600', '700']
+});
+
 export default function Landingpage() {
 
     const [click, setClick] = useState(false);
+    const [click2, setClick2] = useState(false);
+    const [click3, setClick3] = useState([0, 0, 0]);
+    const [warning, setWarning] = useState(true);
+    const [select, setSelect] = useState(false);
     const [transition, setTransition] = useState('');
     const [data, setData] = useState([]);
-    const [x, setX] = useState(0);
-    const [y, setY] = useState(0);
+    const [index, setIndex] = useState(0);
     const [written, setWritten] = useState('')
     const [val, setVal] = useState('')
+    const [country, setCountry] = useState('')
 
     const dispatch = useDispatch();
     const router = useRouter();
@@ -57,12 +72,13 @@ export default function Landingpage() {
             setVal('');
         }
         setWritten(value)
+        setVal(value)
         if (value !== '') {
             const response = await axios.get(`${process.env.NEXT_PUBLIC_MAPBOX_URL}/suggest?q=${value}&language=en&session_token=${process.env.NEXT_PUBLIC_MAPBOX_SESSION_SECRET}&access_token=${process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}`)
             if (response.status === 200) {
                 const cities = response.data.suggestions
                 setData(cities);
-                // console.log(data)
+                console.log(data)
             } else {
                 console.log("error aagya")
             }
@@ -71,8 +87,10 @@ export default function Landingpage() {
 
     async function handleSelect(x, y) {
         setVal(x.name)
+        setCountry(x.place_formatted)
+        setSelect(true)
         dispatch(storeCity({ city: x.name }))
-        const response = await axios.get(`https://api.mapbox.com/search/searchbox/v1/retrieve/${y}?session_token=${process.env.NEXT_PUBLIC_MAPBOX_SESSION_SECRET}&access_token=${process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}`)
+        const response = await axios.get(`${process.env.NEXT_PUBLIC_MAPBOX_URL}/retrieve/${y}?session_token=${process.env.NEXT_PUBLIC_MAPBOX_SESSION_SECRET}&access_token=${process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}`)
         if (response.status === 200) {
             const data = response.data.features;
             data.map(x => {
@@ -90,25 +108,50 @@ export default function Landingpage() {
 
     function handleSurf() {
         if (val !== '') {
-            router.push('/resturants')
+            const r = country.substring(country.length - 5, country.length)
+            if (r !== 'India') {
+                setClick2(true)
+            } else {
+                router.push('/resturants')
+            }
+        } else {
+            setWarning(false);
         }
     }
 
+    function clickfun(i) {
+        const newclick = [...click3];
+        newclick[i] === 0 ? newclick[i] = 1 : newclick[i] = 0;
+        newclick.forEach((x, y) => {
+            if (y !== i) {
+                newclick[y] = 0
+            }
+        })
+        setClick3(newclick);
+        console.log(newclick)
+    }
+
+
     return (
         <div>
-            <div className={`${railway.className} bg h-[100vh] w-[100vw] flex flex-col`}>
+            <div className={`${railway.className} bg h-[100vh] w-[110vw] sm:w-[100vw] flex flex-col`}>
                 <DefaultLayout />
                 <div className='mr-6 ml-6'>
 
                     <div className={`flex justify-center mt-8 text-5xl text-center`}>
                         <h1 className={styles.h1}> Locally Produced <br /> Delivered Direct to <br /> Your Door </h1>
                     </div>
-                    <div className='flex justify-center mt-2 text-gray-600 text-center text-sm'>
+                    <div className='hidden sm:visible sm:flex justify-center mt-2 text-gray-600 text-center text-sm'>
                         <p> Browse your favourite food or restaurant near you. add to cart. don't pay for <br /> any of those items.
                             get it all saved in your account. all of this being <br /> powered by mongodb, redux and next
                         </p>
                     </div>
-                    <div className='flex justify-center ml-[3rem]'>
+                    <div className='sm:hidden visible flex justify-center mt-8 text-gray-600 text-center text-sm'>
+                        <p className='leading-6'> Browse your favourite food or restaurant near you. add to <br /> cart. don't pay  any of those items.
+                            get it all saved in <br /> your account. all of this being powered by <br />mongodb, redux and next
+                        </p>
+                    </div>
+                    <div className='flex justify-center sm:ml-[2rem] sm:mt-0 mt-8 -ml-4'>
                         <div className='flex'>
                             <div className='bg-white h-11 w-10 mt-6 -ml-8 absolute rounded-md'>
 
@@ -122,37 +165,41 @@ export default function Landingpage() {
                             }
                             <div className='flex'>
 
-                                <div className='flex flex-col'>
+                                <div className='flex flex-col w-[16rem]'>
                                     <input
-                                        placeholder='search any place (only india pls)'
-                                        className='w-[18rem] rounded-md p-3 mt-6 text-sm focus:outline-none'
+                                        placeholder={warning ? `search any place (only india pls)` : `HEY! Put some value FIRST!`}
+                                        className=' rounded-md p-3 mt-6 text-sm focus:outline-none transition-all'
                                         onClick={() => clicked()}
                                         onBlur={() => clicked()}
                                         onChange={(e) => handleSearch(e)}
-                                        contentEditable={val}
+                                        value={!click2 ? val : 'no city out of india pls'}
                                     />
-                                    {
-                                        data.length > 0 && written !== '' && val === '' ? data.map(
-                                            (x, i) => <div className="-ml-6 mt-1" key={i}>
-                                                {
-                                                    x.feature_type === 'place' || x.feature_type === 'locality' ?
-                                                        <>
-                                                            <button
-                                                                className=""
-                                                                onClick={() => handleSelect(x, x.mapbox_id)}
-                                                            >
-                                                                {x.name}, {x.place_formatted}
-                                                            </button>
-                                                        </>
-                                                        : <></>
-                                                }
-                                            </div>
-                                        ) : ""
-                                    }
+                                    <div className='flex flex-col transition-all ease-in'>
+
+                                        {
+                                            data.length > 0 && written !== '' && !select ? data.map(
+                                                (x, i) => <div className="transition-all ease-in flex flex-col mt-4" key={i}>
+                                                    {
+
+                                                        x.feature_type === 'place' || x.feature_type === 'locality' ?
+                                                            <div>
+                                                                <button
+                                                                    className="text-start text-sm rounded-md transition-all ease-in"
+                                                                    onClick={() => handleSelect(x, x.mapbox_id)}
+                                                                >
+                                                                    {x.name}, {x.place_formatted}
+                                                                </button>
+                                                            </div>
+                                                            : <></>
+                                                    }
+                                                </div>
+                                            ) : ""
+                                        }
+                                    </div>
                                 </div>
                                 <div>
                                     <button
-                                        className='p-2 bg-transparent hover:bg-[#e3f3d6] transition-all mt-8 ml-0 w-12 sm:w-16 rounded-md'
+                                        className='p-2 bg-transparent hover:bg-[#e3f3d6] transition-all mt-8 ml-2 sm:ml-0 w-12 sm:w-16 rounded-md absolute'
                                         onClick={() => handleSurf()}
                                     >
                                         Go
@@ -163,17 +210,108 @@ export default function Landingpage() {
                     </div>
                 </div>
             </div>
-            <div className='flex justify-around'>
-                <div>
-                    <div className={`${railway.className} p-12`}>
-                        <p className='text-5xl'> Who we are and <br /> What we do </p>
-                    </div>
-                    <div>
-                        {text.map(x => <p> {x.title} </p>)}
+            <div className={`${kanit.className} font-bold sm:flex sm:flex-row flex flex-col`}>
+                <div className='bg-[#cefdcc] w-[110vw] sm:w-1/2'>
+                    <div className='ml-8 sm:ml-24'>
+                        <div className={`p-12 sm:p-12 flex flex-col`}>
+                            <p className='text-5xl text-[#063519]'> Who we are and <br /> What we do </p>
+                            <p className='text-sm text-gray-600 mt-4'> It also supplies nutrients that help maintain the brain, muscles, bones, nerves, skin, blood circulation, and immune system. </p>
+                        </div>
+                        <div>
+                            <div className='p-8 sm:p-12 -mt-12 sm:mb-0 mb-12'>
+                                <div>
+                                    {text.map((x, i) => <div
+                                        className={`flex flex-col p-4 ${click3[i] === 1 ? `bg-[#cefdcc] text-[#2d5c3c]` : `bg-[#80c17e8f] text-white`}   rounded-md hover:bg-[#cefdcc] hover:text-[#2d5c3c] transition-all hover:cursor-pointer mt-6 justify-center`}
+                                        onClick={() => (setIndex(x.id), clickfun(i))}
+                                    >
+                                        <div className='flex'>
+                                            <div>
+                                                <Image
+                                                    src={x.dp}
+                                                    height={20}
+                                                    width={40}
+                                                    alt={'an image just so wow'}
+                                                    className='rounded-md'
+                                                />
+                                            </div>
+                                            <div className='mt-2 ml-4'>
+                                                <p> {x.title} </p>
+                                                <p className='font-medium text-sm'> {x.smalltitls} </p>
+                                            </div>
+                                        </div>
+                                    </div>)}
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className='p-12 text-4xl mt-24'>
-                    dyanmic content on click
+                <div className={`${styles.bg} w-[110vw] sm:p-0 sm:w-1/2`}>
+                    <div className={`${styles.imageclass} p-6 mt-4 sm:p-12 text-4xl sm:-mt-2 sm:ml-20`}>
+                        <Image
+                            src={text[index].image}
+                            height={600}
+                            width={620}
+                            alt={'an image just so wow'}
+                            className={`rounded-md`}
+                        />
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <div className='flex p-12'>
+                        <div className={`h-10 w-10 ${styles.pattern} mt-8`}>
+
+                        </div>
+                        <div>
+                            <p className={`${railway.className} text-5xl`}> COMMENTS </p>
+                        </div>
+                        <div className={`h-10 w-10 ${styles.pattern} -mt-8`}>
+
+                        </div>
+                    </div>
+                    <div>
+
+                    </div>
+                </div>
+
+            </div>
+            <div className='w-[110vw] sm:w-[100vw]'>
+                <div className='flex space-x-6 justify-around p-12'>
+                    <div className='flex flex-col space-y-4'>
+                        <p className={`tracking-normal text-[#2d5c3c] text-2xl font-bold ${comfortaa.className}`}> carefood</p>
+                        <div className='flex flex-col'>
+                            <p> Find us on </p>
+                            <div className='flex space-x-4 mt-2 '>
+                                <Instagram />
+                                <Twitter />
+                                <Facebook />
+                                <Mail />
+                            </div>
+                        </div>
+                    </div>
+                    <div className={`${kanit.className} flex flex-col`}>
+                        <p className='text-xl'> Company </p>
+                        <div className='space-y-4 mt-2'>
+                            <p> About Us </p>
+                            <p> Contact Us </p>
+                            <p> Careers </p>
+                        </div>
+                    </div>
+                    <div className={`${kanit.className} hidden sm:visible flex flex-col`}>
+                        <p className='text-xl'> Assistance </p>
+                        <div className='space-y-4 mt-2'>
+                            <p> Terms and Conditions </p>
+                            <p> Privacy Policy </p>
+                        </div>
+                    </div>
+                    <div className={`${kanit.className} flex flex-col`}>
+                        <p className='text-xl'> Services </p>
+                        <div className='space-y-4 mt-2'>
+                            <p> Find a restaurant </p>
+                            <p> Contact our god </p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
